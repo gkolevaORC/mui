e305c946cb294b2cabd17f6130e7f6fa516e6b3f
gabrielakoleva99 <gabriela.koleva991@gmail.com>
2024-03-27 12:56:31 +0100

    listen to backend

--- backend/server.js
+++ backend/server.js
@@ -56,6 +56,6 @@ app.get("/*", async function(req, res){
 
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
